93cd9e45c06a87ce8d46354e210d537365717eb1
JORGE LUIS <jlcorrales@puce.edu.ec>
2026-06-07 12:35:05 -0500

    Create and prepend global CorsMiddleware to allow cross-origin preflights and error responses

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -12,6 +12,7 @@ return Application::configure(basePath: dirname(__DIR__))
         health: '/up',
     )
     ->withMiddleware(function (Middleware $middleware): void {
+        $middleware->prepend(\App\Http\Middleware\CorsMiddleware::class);
         $middleware->web(append: [
             \App\Http\Middleware\HandleInertiaRequests::class,
         ]);
