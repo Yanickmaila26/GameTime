be3e7cb9d53d8eb8125489a3dd66ad4873666487
Yanickmaila26 <aetherin12@gmail.com>
2026-06-15 15:39:29 -0500

    fix: remove redundant middleware prepend and adjust database URL fallback

--- config/database.php
+++ config/database.php
@@ -85,7 +85,7 @@ return [
 
         'pgsql' => [
             'driver' => 'pgsql',
-            'url' => env('DB_URL'),
+            'url' => env('DB_URL') ?: env('db_url'),
             'host' => env('DB_HOST', '127.0.0.1'),
             'port' => env('DB_PORT', '5432'),
             'database' => env('DB_DATABASE', 'laravel'),
